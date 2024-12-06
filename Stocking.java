ee48c315df9a0f66abf212752fe9edec9147f179
Si-man-65 <sheyman25@bfacademy.de>
2024-12-06 12:44:56 +0000

    Test

--- Stocking.java
+++ Stocking.java
@@ -5,12 +5,42 @@ public class Stocking
     private int itemCount; //Number of items in a stocking
     private int idNum; //ID Number of a stocking
 
+    private static int stockingCount = 0; //Static variables belong to the class
+    public Stocking()
+    {
+        owner = "";
+        itemCount = 0;
+        stockingCount++;
+        idNum = stockingCount;
+    }
+    public Stocking (String name, int initItems)
+    {
+        owner = name;
+        itemCount = initItems;
+        idNum = ++stockingCount;
+    }
+    public int getitemCount()
+    {
+        return itemCount;
+    }
+    public String getOwner()
+    {
+        return owner;
+    }
+    public int getID()
+    {
+        return idNum;
+    }
 }
-class Testlass()
+class TestStocking()
 {
     public static void main(String[] args)
     {
-        Stocking myStocking = new Stocking();
+        Stocking s1 = new Stocking();
+        Stocking s2 = new Stocking("Silas", 4);
+        Stocking s3 = new Stocking("Micah", 2);
+
+        s2.getitemCount();
     }
 
 }
